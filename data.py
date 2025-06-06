681f2cb7f46d095bdaf28e90fa8d7c14d8b9d657
CentralLoaf <hdwater2@gmail.com>
2025-06-05 22:12:35 -0700

    Minor data formatting

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 import yfinance as yf
 import pandas as pd
-import utils
 
 tickers = [
     # Technology
@@ -17,13 +16,13 @@ tickers = [
     'XOM', 'CVX', 'COP', 'SLB', 'PSX', 'EOG', 'VLO', 'MPC', 'KMI'
 ]
 
-data = yf.download(tickers, start='2000-01-01', end='2025-6-1', interval='1d')
-df = pd.DataFrame()
-
-print(data.columns.values)
 # Downoad ohlcv data
-for i in range(1, 60):
-    df = pd.concat(df, utils.parse_data(data))
-    print(ticker)
-    
+data = yf.download(tickers, start='2000-01-01', end='2025-6-1', interval='1d')
+df = pd.DataFrame('date': [], 'ticker': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': [])
+
+mapping_dict = {}
+
+for ticker in tickers:
+    df
+
 data.to_csv('data.csv')
