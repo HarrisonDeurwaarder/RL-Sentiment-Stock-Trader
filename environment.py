cb432c697b82d18318c2c9caa5d9ae2ed168af13
CentralLoaf <hdwater2@gmail.com>
2025-06-11 21:53:43 -0700

    Minor misc edits

--- environment.py
+++ environment.py
@@ -5,7 +5,9 @@ import pandas as pd
 
 class Environment:
     
-    def __init__(self, ticker: str,) -> None:
+    def __init__(self, 
+                 ticker: str,
+        ) -> None:
         self.ticker = ticker
         self.date = dt.datetime(2000, 1, 1)
         self.df = pd.DataFrame()
@@ -24,11 +26,27 @@ class Environment:
         self.df = self.df.rename(columns={'Price': 'date', 'Close': 'close', 'High': 'high', 'Low': 'low', 'Open': 'open', 'Volume': 'volume'})
     
     
-    def step(self, max_memory: int = 20) -> torch.Tensor:
-        is_over = self.index >= len(self.df)-1
+    def get_states(self, 
+                   index: int, 
+                   max_memory: int = 20,
+        ) -> torch.Tensor:
+        return self.df.iloc[max(index - max_memory+1, 0): index+1] if index > len(self.df) else None
+    
+    
+    def get_reward(self,
+                   index: int,
+        ) -> float:
+        pass
+    
+    
+    def step(self, 
+             max_memory: int = 20,
+        ) -> torch.Tensor:
+        is_over = self.index > len(self.df)-1
 
         # Get the past (max_memory) states, or the most if that's not possible
-        states = self.df.iloc[max(self.index - max_memory+1, 0): self.index+1]
+        states = self.get_states(index=self.index, max_memory=max_memory)
+        next_states = self.get_states(index=self.index+1, max_memory=max_memory)
         
         self.index += 1
         return states, is_over
