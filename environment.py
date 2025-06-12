82636289c343c3882e55f7a43c6c1668861e3c7b
CentralLoaf <hdwater2@gmail.com>
2025-06-11 22:38:28 -0700

    Preliminary reward function considerations

--- environment.py
+++ environment.py
@@ -9,6 +9,9 @@ class Environment:
                  ticker: str,
         ) -> None:
         self.ticker = ticker
+        
+        self.shares = [0]
+        
         self.date = dt.datetime(2000, 1, 1)
         self.df = pd.DataFrame()
         self.index = 0
@@ -20,6 +23,9 @@ class Environment:
     
     
     def assemble_env(self,) -> None:
+        '''
+        Loads the dataframe from an imported CSV file
+        '''
         # Extract the historical data from a single company
         data = pd.read_csv(f'Data/{self.ticker}_ohlcv.csv')
         self.df = data.drop([0, 1])
@@ -30,18 +36,39 @@ class Environment:
                    index: int, 
                    max_memory: int = 20,
         ) -> torch.Tensor:
+        '''
+        Given an index, returns the current state and memory states
+        '''
         return self.df.iloc[max(index - max_memory+1, 0): index+1] if index > len(self.df) else None
     
     
     def get_reward(self,
                    index: int,
         ) -> float:
-        pass
+        '''
+        Gets the reward of the current state
+        '''
     
     
-    def step(self, 
+    @staticmethod
+    def get_port_val(shares: int,
+                     high: float,
+                     low: float,
+        ) -> float:
+        '''
+        Gets the value of the portfolio given the average price and num shares
+        '''
+        avg_price = (high + low) * 0.5
+        return shares * avg_price
+    
+    
+    def step(self,
+             action: float,
              max_memory: int = 20,
         ) -> torch.Tensor:
+        '''
+        Handles a step in the RL environment (executes action, returns next state, computes reward)
+        '''
         is_over = self.index > len(self.df)-1
 
         # Get the past (max_memory) states, or the most if that's not possible
